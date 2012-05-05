379c0964ff16d9a61c03e84cf0742906511d3719
neyder angarita <neyder6101@hotmail.com>
2012-05-05 18:01:05 -0500

    actualizacion formulario experiencia laboral

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 /* Author:
-
+		alert("Has pulsado el enlace...nAhora serás enviado a DesarrolloWeb.com");
 */
-
-
-
-
-
+$(document).ready(function(){
+   $("#prueba").click(function(evento){
+		var elem =$("#formulariomayores");
+		/*elem.css("display", "block");*/
+		elem.show(3000);
+   });
+});
